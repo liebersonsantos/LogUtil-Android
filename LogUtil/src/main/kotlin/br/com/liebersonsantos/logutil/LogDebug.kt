203c1caaef7f5abf2011aa5008f339136b357632
Lieberson  Xavier dos Santos <lieberson@lieberson.local>
2020-09-28 18:58:11 -0300

    fix debug

--- LogUtil/src/main/kotlin/br/com/liebersonsantos/logutil/LogDebug.kt
+++ LogUtil/src/main/kotlin/br/com/liebersonsantos/logutil/LogDebug.kt
@@ -6,9 +6,9 @@ open class LogDebug {
 
     companion object{
         private const val TAG = "LOG_UTIL_DEBUG"
-    }
 
-    fun debug(message: String){
-        Log.d(TAG, message)
+        fun debug(message: String){
+            Log.d(TAG, message)
+        }
     }
 }
